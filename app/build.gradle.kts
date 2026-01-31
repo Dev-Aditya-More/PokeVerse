480c2d2e09cb55f498e553206cf8a42aeb6c5135
Aditya More <aditya1875more@gmail.com>
2026-01-31 13:50:47 +0530

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = 25
         targetSdk = 36
 
-        versionCode = 41
-        versionName = "1.4.1"
+        versionCode = 42
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
