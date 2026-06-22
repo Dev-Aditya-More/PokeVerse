8f1a1e85ef77ab52761dccf6c19d2beedf0ea8a6
Aditya More <aditya1875more@gmail.com>
2026-06-22 17:19:13 +0530

    bump the Gradle version to 1.7.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 25
         targetSdk = 36
 
-        versionCode = 77
-        versionName = "1.7.7"
+        versionCode = 78
+        versionName = "1.7.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
